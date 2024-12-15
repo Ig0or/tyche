7d2fac23e10249ac79e0feaadc58138f65d49ea5
Ig0or <igorprovensi@gmail.com>
2024-12-14 22:34:03 -0300

    feat: update database config with ioc injection

--- src/externals/infrastructure/database_config/postgres_database_config.go
+++ src/externals/infrastructure/database_config/postgres_database_config.go
@@ -2,17 +2,26 @@ package database_config
 
 import (
 	"context"
+	"github.com/Ig0or/tyche/src/externals/ports/infrastructure/i_logger_config"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"log"
+	"go.uber.org/dig"
 	"os"
 )
 
 type PostgresDatabaseConfig struct {
 	connectionPool *pgxpool.Pool
+
+	logger i_logger_config.ILoggerConfig
 }
 
-func NewPostgresDatabaseConfig() *PostgresDatabaseConfig {
-	database := &PostgresDatabaseConfig{}
+type PostgresDatabaseConfigDependencies struct {
+	dig.In
+
+	Logger i_logger_config.ILoggerConfig `name:"LoggerConfig"`
+}
+
+func NewPostgresDatabaseConfig(dependencies PostgresDatabaseConfigDependencies) *PostgresDatabaseConfig {
+	database := &PostgresDatabaseConfig{logger: dependencies.Logger}
 
 	database.openConnection()
 
@@ -27,7 +36,7 @@ func (database *PostgresDatabaseConfig) openConnection() {
 	defer connectionPool.Close()
 
 	if err != nil {
-		log.Fatal(err)
+		database.logger.Fatal("Fail to open database connection - Error: " + err.Error())
 	}
 
 	database.connectionPool = connectionPool
