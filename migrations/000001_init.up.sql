c19091803e28e30f49210a937b001b6a5065067e
Ig0or <igorprovensi@gmail.com>
2024-12-26 23:40:52 -0300

    fix: types of database migration

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS accounts (
-    id INT NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     account_id UUID NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    cpf VARCHAR(11) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     password VARCHAR(150) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
