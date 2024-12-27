c9427914760fd128d2f7d79a8fc3585e629f86d0
Ig0or <igorprovensi@gmail.com>
2024-12-26 23:42:00 -0300

    feat: finished "create account" presenter

--- src/adapters/presenters/account/create_account_presenter.go
+++ src/adapters/presenters/account/create_account_presenter.go
@@ -1,10 +1,13 @@
 package presenters
 
 import (
-	"github.com/Ig0or/tyche/src/adapters/presenters"
 	"github.com/Ig0or/tyche/src/application/data_types/requests/account"
 	"github.com/Ig0or/tyche/src/domain/custom_errors"
+	"github.com/Ig0or/tyche/src/domain/entities"
+	"github.com/Ig0or/tyche/src/domain/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"time"
 )
 
 type CreateAccountPresenter struct {
@@ -20,10 +23,39 @@ func (presenter *CreateAccountPresenter) FromContextToRequest(context *gin.Conte
 	err := context.ShouldBindJSON(&request)
 
 	if err != nil {
-		customError := presenters.FormatRequestValidationError(err)
+		messageError := custom_errors.FormatRequestValidationError(err)
+		customError := custom_errors.NewBadRequestError("Validation error. Some fields are invalid", messageError)
 
-		return &request, customError
+		return nil, customError
 	}
 
 	return &request, nil
+}
+
+func (presenter *CreateAccountPresenter) FromRequestToEntity(request *requests.CreateAccountRequest) (*entities.AccountEntity, *custom_errors.BaseCustomError) {
+	err := entities.VerifyValidCpf(request.Cpf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	hashedPassword, err := entities.HashPassword(request.Password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	accountId := uuid.New()
+
+	accountEntity := &entities.AccountEntity{AccountId: accountId, Email: request.Email, Cpf: request.Cpf, Password: hashedPassword}
+
+	return accountEntity, nil
+}
+
+func (presenter *CreateAccountPresenter) FromEntityToModel(entity *entities.AccountEntity) *models.AccountModel {
+	currentTime := time.Now().UTC()
+
+	accountModel := &models.AccountModel{AccountId: entity.AccountId, Email: entity.Email, Cpf: entity.Cpf, Password: entity.Password, CreatedAt: currentTime, UpdatedAt: currentTime}
+
+	return accountModel
 }
--- src/application/ports/presenters_interface/account/create_account_presenter_interface.go
+++ src/application/ports/presenters_interface/account/create_account_presenter_interface.go
@@ -3,9 +3,13 @@ package presenters_interface
 import (
 	"github.com/Ig0or/tyche/src/application/data_types/requests/account"
 	"github.com/Ig0or/tyche/src/domain/custom_errors"
+	"github.com/Ig0or/tyche/src/domain/entities"
+	"github.com/Ig0or/tyche/src/domain/models"
 	"github.com/gin-gonic/gin"
 )
 
 type CreateAccountPresenterInterface interface {
 	FromContextToRequest(context *gin.Context) (*requests.CreateAccountRequest, *custom_errors.BaseCustomError)
+	FromRequestToEntity(request *requests.CreateAccountRequest) (*entities.AccountEntity, *custom_errors.BaseCustomError)
+	FromEntityToModel(entity *entities.AccountEntity) *models.AccountModel
 }
